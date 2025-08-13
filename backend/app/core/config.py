d5d4d0bd2565986ea96738f937ed7248a2a5131c
Kushan7 <carchana813@gmail.com>
2025-08-13 16:17:23 +0530

    Added secret

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,9 +4,13 @@ from dotenv import load_dotenv
 # This line loads the .env file from your 'backend' directory
 load_dotenv()
 
+# In config.py
 class Settings:
     DATABASE_URL: str = os.getenv("DATABASE_URL")
     PROJECT_NAME: str = "Billboard Watch"
     API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = os.getenv("SECRET_KEY") # Add this
+    ALGORITHM: str = os.getenv("ALGORITHM") # Add this
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)) # Add this
 
 settings = Settings()

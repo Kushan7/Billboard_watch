a57f59964d0c10d916f80b7ebe4e80c9d1022f77
Kushan7 <carchana813@gmail.com>
2025-08-13 16:18:58 +0530

    Added access token

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,5 +1,7 @@
 from passlib.context import CryptContext
-
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from app.core.config import settings
 # Create a CryptContext instance, specifying the hashing algorithm
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -10,3 +12,11 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def get_password_hash(password: str) -> str:
     """Hashes a plain password."""
     return pwd_context.hash(password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt

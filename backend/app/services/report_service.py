275514de242a14a4715f88265edf33c19e6e30e6
Kushan7 <carchana813@gmail.com>
2025-08-14 12:10:40 +0530

    Saves report in db

---
+++ backend/app/services/report_service.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Session
+from app.db import models
+from app.schemas import report as report_schema
+
+def create_report(db: Session, report: report_schema.ReportCreate, user_id: uuid.UUID):
+    """
+    Creates a new report in the database linked to a user.
+    """
+    db_report = models.Report(
+        latitude=report.latitude,
+        longitude=report.longitude,
+        user_id=user_id
+    )
+    db.add(db_report)
+    db.commit()
+    db.refresh(db_report)
+    return db_report

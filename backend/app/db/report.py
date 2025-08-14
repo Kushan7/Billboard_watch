4ca75644c0c2b50e299ab9ede3df0cc1ec7695f6
Kushan7 <carchana813@gmail.com>
2025-08-14 12:09:22 +0530

    Adding report in db

---
+++ backend/app/db/report.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+import uuid
+from datetime import datetime
+
+# Data received from the user when creating a report
+class ReportCreate(BaseModel):
+    latitude: float
+    longitude: float
+
+# Data returned to the user after a report is created
+class Report(BaseModel):
+    report_id: uuid.UUID
+    user_id: uuid.UUID
+    timestamp: datetime
+    latitude: float
+    longitude: float
+    status: str
+
+    class Config:
+        from_attributes = True # Replaces orm_mode = True
